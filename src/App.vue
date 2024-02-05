1c957523e61ee969a788649cb18587376391c5a1
asiloprince <asilo.prince25@gmail.com>
2024-02-05 23:29:06 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div>
+      <h1>Pokedex</h1>
+      <input type="text" placeholder="Search by name" />
+      <button>Search</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+div {
+  text-align: center;
+}
+</style>
